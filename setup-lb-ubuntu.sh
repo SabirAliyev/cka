73f4ed981e44caeb3d63b89f3be0df5f3a3e394d
Sabir Aliyev <sabir.aliyev.itdep@gmail.com>
2026-05-07 05:53:11 -0400

    adapt LB config. Attempt 1

--- setup-lb-ubuntu.sh
+++ setup-lb-ubuntu.sh
@@ -1,141 +1,74 @@
 #!/bin/bash
-#
-# source https://github.com/sandervanvugt/cka/setup-lb.sh
+# Refactored for CP-11, CP-12, CP-13 on enp1s0
+# Target VIP: 192.168.122.100
 
-# script to set up load balancing on cluster nodes
-# for use in CKA courses by Sander van Vugt
-# version 0.7
-# currently only tested on Ubuntu 22.04 LTS Server
-# run this AFTER running setup-container.sh and setup-kubetools.sh
-#
-# TODO: remove the many password prompts
+# Node variables
+CP1="cp-11"
+CP2="cp-12"
+CP3="cp-13"
+INTERFACE="enp1s0"
+VIP="192.168.122.100"
 
-if which kubectl
-then
-	echo all good moving on
-else
-	echo please run setup-container.sh and setup-kubetools.sh first and then run this again
-	exit 6
+# 1. Pre-flight Check
+if ! which kubectl > /dev/null; then
+    echo "Kube-tools missing. Run setup-container.sh and setup-kubetools.sh first."
+    exit 6
 fi
 
-## establish key based SSH with remote hosts
-# obtain node information
-if grep control1 /etc/hosts | grep -v 127
-then
-	export CONTROL1_IP=$(awk '/control1/ { print $1 }' /etc/hosts | grep -v 127)
-else
-	echo enter IP address for control1
-	read CONTROL1_IP
-	export CONTROL1_IP=$CONTROL1_IP
-	sudo sh -c "echo $CONTROL1_IP control1 >> /etc/hosts"
-fi
+# 2. Extract IPs from /etc/hosts
+export CP1_IP=$(awk -v node="$CP1" '$2==node {print $1}' /etc/hosts | head -1)
+export CP2_IP=$(awk -v node="$CP2" '$2==node {print $1}' /etc/hosts | head -1)
+export CP3_IP=$(awk -v node="$CP3" '$2==node {print $1}' /etc/hosts | head -1)
 
-
-if grep control2 /etc/hosts | grep -v 127
-then
-        export CONTROL2_IP=$(awk '/control2/ { print $1 }' /etc/hosts | grep -v 127)
-else
-        echo enter IP address for control2
-        read CONTROL2_IP
-        export CONTROL2_IP=$CONTROL2_IP
-        sudo sh -c "echo $CONTROL2_IP control2 >> /etc/hosts"
-fi
-
-
-if grep control3 /etc/hosts | grep -v 127
-then
-        export CONTROL3_IP=$(awk '/control3/ { print $1 }' /etc/hosts | grep -v 127)
-else
-        echo enter IP address for control3
-        read CONTROL3_IP
-        export CONTROL3_IP=$CONTROL3_IP
-        sudo sh -c "echo $CONTROL3_IP control3 >> /etc/hosts"
-fi
-
-
-echo ##### READ ALL OF THIS BEFORE CONTINUING ######
-echo this script requires you to run setup-container.sh and setup-kubetools.sh first
-echo this script is based on the NIC name ens33
-echo if your networkcard has a different name, edit keepalived.conf
-echo before continuing and change "interface ens33" to match your config
-echo .
-echo this script will create a keepalived apiserver at 192.168.29.100
-echo if this IP address does not match your network configuration,
-echo manually change the check_apiserver.sh file before continuing
-echo also change the IP address in keepalived.conf
-echo .
-echo press enter to continue or Ctrl-c to interrupt and apply modifications
-read
-
-# performing check on critical files
-for i in keepalived.conf check_apiserver.sh haproxy.cfg
-do
-	if [ ! -f $i ]
-	then
-		echo $i should exist in the current directory && exit 2
-	fi
+# Verify all IPs resolved
+for ip in "$CP1_IP" "$CP2_IP" "$CP3_IP"; do
+    if [ -z "$ip" ]; then echo "Error: IP for $CP1, $CP2, or $CP3 not found in /etc/hosts"; exit 1; fi
 done
 
-# generating and distributing SSH keys
-ssh-keygen
-ssh-copy-id control1
-ssh-copy-id control2
-ssh-copy-id control3
+# 3. Distribute SSH keys for automation
+ssh-keygen -t rsa -N "" -f ~/.ssh/id_rsa <<< n
+for node in $CP1 $CP2 $CP3; do ssh-copy-id "$node"; done[cite: 6]
 
-# configuring sudo for easier access
-sudo sh -c "echo 'Defaults timestamp_type=global,timestamp_timeout=60' >> /etc/sudoers"
-sudo scp -p /etc/sudoers $USER@control2:/tmp/ && ssh -t control2 'sudo -S chown root:root /tmp/sudoers' && ssh -t control2 'sudo -S cp -p /tmp/sudoers /etc/'
-sudo scp -p /etc/sudoers $USER@control3:/tmp/ && ssh -t control3 'sudo -S chown root:root /tmp/sudoers' && ssh -t control3 'sudo -S cp -p /tmp/sudoers /etc/'
-#ssh control2 sudo -S sh -c "echo 'Defaults timestamp_type=global,timestamp_timeout=60' >> /etc/sudoers"
-#ssh control3 sudo -S sh -c "echo 'Defaults timestamp_type=global,timestamp_timeout=60' >> /etc/sudoers"
+# 4. Install Software Stack
+for node in $CP1 $CP2 $CP3; do
+    ssh "$node" "sudo apt update && sudo apt install -y haproxy keepalived"[cite: 1]
+done
 
-# install required software
-sudo apt install haproxy keepalived -y
-ssh control2 "sudo -S apt install haproxy keepalived -y"
-ssh control3 "sudo -S apt install haproxy keepalived -y"
-
-scp /etc/hosts control2:/tmp && ssh -t control2 'sudo -S cp /tmp/hosts /etc/'
-scp /etc/hosts control3:/tmp && ssh -t control3 'sudo -S cp /tmp/hosts /etc/'
-
-# create keepalived config
-# change IP address to anything that works in your environment!
+# 5. Configure keepalived Health Check
 sudo chmod +x check_apiserver.sh
 sudo cp check_apiserver.sh /etc/keepalived/
+for node in $CP2 $CP3; do
+    scp check_apiserver.sh "$node":/tmp/
+    ssh "$node" "sudo cp /tmp/check_apiserver.sh /etc/keepalived/"
+done
 
+# 6. Generate Node-Specific keepalived Configs
+# CP1 is Master; others are Slaves
+cp keepalived.conf keepalived-$CP2.conf
+cp keepalived.conf keepalived-$CP3.conf
 
-scp check_apiserver.sh control2:/tmp && ssh -t control2 'sudo -S cp /tmp/check_apiserver.sh /etc/keepalived'
-scp check_apiserver.sh control3:/tmp && ssh -t control3 'sudo -S cp /tmp/check_apiserver.sh /etc/keepalived'
-
-#### creating site specific keepalived.conf file
-sudo cp keepalived.conf keepalived-control2.conf
-sudo cp keepalived.conf keepalived-control3.conf
-
-sudo sed -i 's/state MASTER/state SLAVE/' keepalived-control2.conf
-sudo sed -i 's/state MASTER/state SLAVE/' keepalived-control3.conf
-sudo sed -i 's/priority 255/priority 254/' keepalived-control2.conf
-sudo sed -i 's/priority 255/priority 253/' keepalived-control3.conf
+sed -i 's/state MASTER/state SLAVE/' keepalived-$CP2.conf keepalived-$CP3.conf
+sed -i 's/priority 255/priority 254/' keepalived-$CP2.conf
+sed -i 's/priority 255/priority 253/' keepalived-$CP3.conf
 
 sudo cp keepalived.conf /etc/keepalived/
-scp keepalived-control2.conf control2:/tmp && ssh -t control2 'sudo -S cp /tmp/keepalived-control2.conf /etc/keepalived/keepalived.conf'
-scp keepalived-control3.conf control3:/tmp && ssh -t control3 'sudo -S cp /tmp/keepalived-control3.conf /etc/keepalived/keepalived.conf'
+scp keepalived-$CP2.conf "$CP2":/tmp/ && ssh "$CP2" "sudo cp /tmp/keepalived-$CP2.conf /etc/keepalived/keepalived.conf"
+scp keepalived-$CP3.conf "$CP3":/tmp/ && ssh "$CP3" "sudo cp /tmp/keepalived-$CP3.conf /etc/keepalived/keepalived.conf"
 
-### rewriting haproxy.cfg with site specific IP addresses
-sudo sed -i s/server\ control1\ 1.1.1.1\:6443\ check/server\ control1\ $CONTROL1_IP\:6443\ check/ haproxy.cfg
-sudo sed -i s/server\ control2\ 1.1.1.2\:6443\ check/server\ control2\ $CONTROL2_IP\:6443\ check/ haproxy.cfg
-sudo sed -i s/server\ control3\ 1.1.1.3\:6443\ check/server\ control3\ $CONTROL3_IP\:6443\ check/ haproxy.cfg
+# 7. Configure HAProxy with Dynamic IPs
+sed -i "s/server control1.*/server $CP1 $CP1_IP:6443 check/" haproxy.cfg
+sed -i "s/server control2.*/server $CP2 $CP2_IP:6443 check/" haproxy.cfg
+sed -i "s/server control3.*/server $CP3 $CP3_IP:6443 check/" haproxy.cfg
 
-# copy haproxy.cfg to destinations
 sudo cp haproxy.cfg /etc/haproxy/
-scp haproxy.cfg control2:/tmp && ssh -t control2 'sudo -S cp /tmp/haproxy.cfg /etc/haproxy/'
-scp haproxy.cfg control3:/tmp && ssh -t control3 'sudo -S cp /tmp/haproxy.cfg /etc/haproxy/'
+for node in $CP2 $CP3; do
+    scp haproxy.cfg "$node":/tmp/
+    ssh "$node" "sudo cp /tmp/haproxy.cfg /etc/haproxy/"
+done
 
-# start and enable services
-sudo systemctl enable keepalived --now
-sudo systemctl enable haproxy --now
-ssh control2 sudo -S systemctl enable keepalived --now
-ssh control2 sudo -S systemctl enable haproxy --now
-ssh control3 sudo -S systemctl enable keepalived --now
-ssh control3 sudo -S systemctl enable haproxy --now
+# 8. Start Services
+for node in $CP1 $CP2 $CP3; do
+    ssh "$node" "sudo systemctl enable --now keepalived haproxy"[cite: 1]
+done
 
-echo setup is now done, please verify
-echo the first node that started the services - normally control1 -  should run the virtual IP address 192.168.29.100
+echo "LB Setup Complete. VIP: $VIP"
